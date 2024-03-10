47daf0022760a421eef59c71d006535986fcb661
sijan67 <sijuichi1@gmail.com>
2024-03-10 07:02:25 -0700

    Added context to summary page

--- Frontend/learnsmart-AI/screens/SummaryPage/SummaryPage.js
+++ Frontend/learnsmart-AI/screens/SummaryPage/SummaryPage.js
@@ -1,46 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, TouchableOpacity, SafeAreaView } from 'react-native';
+import { View, Text, Image, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
 import style from './style';
 import { useAppContext } from '../../context/AppContext';
 import TreeView from 'react-native-final-tree-view';
 import { List } from 'react-native-paper';
+import { useLecture } from '../../context/LectureContext';
 
 const Summary = ({navigation}) => {
   const { setShowWelcome } = useAppContext();
   const [expanded, setExpanded] = React.useState(true);
 
   const handlePress = () => setExpanded(!expanded);
+
  
+  const { lectureSummary } = useLecture();
+  
 
   return (
     <SafeAreaView style={style.container}>
     <Text style={style.title}> Summary History </Text>
 
-    <List.Section title="">
+    <ScrollView style={{ width: '100%' }}> 
+ <List.Section title="">
+        {lectureSummary.map((lecture, index) => (
+          <List.Accordion
+            style={{width: 400}}
+            key={index} // Using index as a key; consider changing if the list is dynamic
+            title={lecture.lectureTitle}
+            titleNumberOfLines={3}
+            left={props => <List.Icon {...props} icon="book" />}
+          >
 
-      <List.Accordion
-        style={{width: 400}}
-        title="Lecture 3Lecture 3Lecture 3Lecture 3Lecture 3Lecture 3 "
-        titleNumberOfLines={3}
-
-        left={props => <List.Icon {...props} icon="book" />
-      }
-        >
         <List.Item         
-        titleNumberOfLines={15}
-        title="Content: Lecture 3Lecture 3Lecturre3Lecturre3Lecturre3Lecturre 3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture 3 "
-        >
+          titleNumberOfLines={15}
+          title={`Original Content: ${lecture.lectureContent}`}
+          >
         </List.Item>
         
         <List.Item         
-        titleNumberOfLines={15}
-        title="Summary: Lecture 3Lecture 3Lecturre3Lecturre3Lecturre3Lecturre 3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture3Lecture 3 "
+        titleNumberOfLines={100}
+        title={`Summary: ${lecture.summary}`}
         >
         </List.Item>
-      </List.Accordion>
+          </List.Accordion>
+        ))}
+      </List.Section>
 
-    </List.Section>
-
+</ScrollView>
 
     <TouchableOpacity
         style={style.button}
@@ -53,7 +59,6 @@ const Summary = ({navigation}) => {
       </TouchableOpacity>
     </SafeAreaView>
   );
-  
 };
 
 export default Summary;
